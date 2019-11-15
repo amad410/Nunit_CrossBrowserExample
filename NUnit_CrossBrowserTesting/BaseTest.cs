582931a0fd66bab575e07abe549f168e9d733ff8
Antwan Maddox <antwan.maddox@agilethought.com>
2019-11-15 14:14:14 -0500

    commit changes

---
+++ NUnit_CrossBrowserTesting/BaseTest.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using NUnit_CrossBrowserTesting.Enumerations;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_CrossBrowserTesting
+{
+    [TestFixture]
+    public class BaseTest
+    {
+        protected IWebDriver _driver;
+        protected BrowserType _type;
+
+        public BaseTest(BrowserType type)
+        {
+            _type = type;
+
+        }
+        public BaseTest()
+        {
+            
+
+        }
+        [SetUp]
+        public void Initialize()
+        {
+            
+            switch (_type)
+            {
+                case BrowserType.Chrome:
+                    _driver = new ChromeDriver();
+                    break;
+                default:
+                    _driver = new InternetExplorerDriver();
+                    break;
+
+
+
+            }
+        }
+        protected IWebDriver BaseDriver
+        {
+            get
+            {
+                return _driver;
+            }
+            set
+            {
+                _driver = value;
+            }
+        }
+    }
+}
---
+++ NUnit_CrossBrowserTesting/TestClass.cs
@@ -0,0 +1,34 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using NUnit_CrossBrowserTesting.Enumerations;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace NUnit_CrossBrowserTesting
+{
+    [TestFixture(BrowserType.Chrome)]
+    public class TestWithMultipleBrowsers : BaseTest
+    {
+        public TestWithMultipleBrowsers(BrowserType type) : base(type) { }
+
+        
+
+        [Test]
+        public void GoogleTest()
+        {
+            BaseDriver.Navigate().GoToUrl("http://www.google.com/");
+            IWebElement query = BaseDriver.FindElement(By.Name("q"));
+            query.SendKeys("Bread" + Keys.Enter);
+            Thread.Sleep(2000);
+            Assert.AreEqual("Bread - Google Search", BaseDriver.Title);
+            BaseDriver.Quit();
+
+        }
+    }
+}
